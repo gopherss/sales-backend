110a37a27d805909e98754b4e69910a08dda49f9
gopherss <sandrogopher@gmail.com>
2026-03-30 19:34:18 -0500

    cors update

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const {
 const app = express();
 app.use(cors({
     // origin: 'http://localhost:5173',
-    origin: 'http://ventasprueba.netlify.app',
+    origin: 'https://ventasprueba.netlify.app/',
     methods: ['POST', 'GET', 'PUT']
 }));
 
